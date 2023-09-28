41871fd5b065f09625f17a3d6497ed389f008bf3
sayo1718 <dai1718lino@gmail.com>
2023-09-28 16:50:55 +0900

    Javascript 販売手数料、販売利益計算

---
+++ app/javascript/packs/item_price.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function(){
+const priceInput = document.getElementById("item-price");
+   //console.log(inputValue);
+   priceInput.addEventListener("input", () => {
+     const addTaxDom = document.getElementById("add-tax-price");
+     // console.log(priceInput.innerHTML);
+     addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
+     const addPofitDom = document.getElementById("profit");
+     // console.log(priceInput.innerHTML);
+     addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
+ })
+});
