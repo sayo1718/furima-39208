f06cbd5fa84cc728cb3cb8ab8bc1835dc6c05125
sayo1718 <dai1718lino@gmail.com>
2023-10-03 15:29:01 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -18,9 +18,20 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
 
   def item_params
     params.require(:item).permit( :image, :item_name, :explanation, :category_id, :item_condition_id, :delivery_charge_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+  
 end
