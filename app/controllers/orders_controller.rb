8c70b8cca9fbc3ae5eac6ead0236c50320b24898
sayo1718 <dai1718lino@gmail.com>
2023-10-09 19:38:57 +0900

    修正４

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :non_purchased_item, only: [:index, :create]
-  before_action :set_item, only: [:index, :create, :non_purchased_item]
+  before_action :set_item, only: [:index, :create]
 
   def index
     @order_form = OrderForm.new
@@ -25,6 +25,7 @@ class OrdersController < ApplicationController
   end
 
   def non_purchased_item
+    @item = Item.find(params[:item_id])
     redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 
