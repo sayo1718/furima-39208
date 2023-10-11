10e5f1b86a3a824a30b95df4f81336abb21d2266
sayo1718 <dai1718lino@gmail.com>
2023-10-11 23:04:08 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,11 +12,11 @@ class Item < ApplicationRecord
 
   validates :image, presence: true, unless: :was_attached?
   validates :item_name, presence: true
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :item_condition_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
   validates :explanation, presence: true
 
